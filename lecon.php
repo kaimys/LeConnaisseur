16ee3966805c2d0a0276e874a615481b7f2ee328
Christoph <ziegler@irt.de>
2014-09-20 22:27:09 +0200

    added filter to lecon.php

--- lecon.php
+++ lecon.php
@@ -2,6 +2,15 @@
 header("Content-type: application/json");
 $API = "http://tvhackday.api.lab.watchmi.tv";
 
+function now () {
+	return date("Y-m-d")."T".date("H:i:s", time()-60*60*2);
+}
+
+function than ($delay) {
+	$offset=time()-60*60*2+$delay;
+	return date("Y-m-d", $offset)."T".date("H:i:s", $offset);
+}
+
 function watchmi($service, $obj) {
     global $API;
     $url = $API.$service."?";
@@ -35,20 +44,43 @@ switch ($_GET["q"]) {
             'multiSorting' => array(
                 array('criterion' => 'byTime')
             ),
-            'filter' => array('term' => 'sourceId', 'value' => 774),
+            'filter' => array('term' => 'sourceId', 'value' => $_GET["ch"]),
             'annotation' => 'full',
             'windowOfAvailabilityType' => 'strictStart',
             'descendingDefaultByTime' => 'true',
-            'windowOfAvailabilityStart' => '2014-09-20T13:00:00Z',
-            'windowOfAvailabilityEnd' => '2014-09-20T15:45:00Z'
+            'windowOfAvailabilityStart' => now(),
+            'windowOfAvailabilityEnd' => than(60*60*2)
         );
         $response = watchmi("/asset", $query);
+        $response = filter($response);
         break;
 
     case "test":
         $arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
         $response = json_encode($arr);
         break;
+}
+
+function filter ($jsonString) {
+	$response = json_decode($jsonString);
+	
+	foreach ($response as $elem) {
+		$elem = array(
+				"title" => $elem->asset->titles->deu,
+				"assetId" => $elem->asset->assetId,
+				"sourceId" => $elem->asset->sourceId,
+				"imageURL" => $elem->asset->imageUrl[0]);
+		$array[] = $elem;
+	}
+	
+	$response = json_encode($array);
+	
+	if(false) {
+		return $response;
+	} else {
+		return $jsonString;
+	}
+	
 }
 
 echo($response);
