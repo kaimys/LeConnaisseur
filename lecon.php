c7e4a91a8dd5a86dd5deabfa24811cf9499b0764
Kai Mysliwiec <kai.mysliwiec@prosiebensat1digital.de>
2014-09-20 19:39:40 +0200

    Added JSON encoding

--- lecon.php
+++ lecon.php
@@ -1,11 +1,54 @@
 <?php
-$query = $_GET["q"];
-switch ($query) {
+header("Content-type: application/json");
+$API = "http://tvhackday.api.lab.watchmi.tv";
+
+function watchmi($service, $obj) {
+    global $API;
+    $url = $API.$service."?";
+    foreach ($obj as $key => $value) {
+        if(gettype($value) == "array") {
+            $url .= $key."=".urlencode(json_encode($value))."&";
+        } else {
+            $url .= $key."=".$value."&";
+        }
+    }
+    if(true) {
+        return file_get_contents($url);
+    } else {
+        return $url;
+    }
+}
+
+switch ($_GET["q"]) {
+
     case "channels":
-        $q = "http://tvhackday.api.lab.watchmi.tv/documentstore/sources?filter={%22term%22:%22assetTypes%22,%22value%22:%22broadcast%22}";
+        $query = array(
+            'filter' => array('term' => 'assetTypes', 'value' => 'broadcast')
+        );
+        $response = watchmi("/documentstore/sources", $query);
+        break;
+
+    case "nownext":
+        $query = array(
+            'numberOfItems' => 10,
+            'sorting' => 'multi',
+            'multiSorting' => array(
+                array('criterion' => 'byTime')
+            ),
+            'filter' => array('term' => 'sourceId', 'value' => 774),
+            'annotation' => 'full',
+            'windowOfAvailabilityType' => 'strictStart',
+            'descendingDefaultByTime' => 'true',
+            'windowOfAvailabilityStart' => '2014-09-20T13:00:00Z',
+            'windowOfAvailabilityEnd' => '2014-09-20T15:45:00Z'
+        );
+        $response = watchmi("/asset", $query);
+        break;
+
+    case "test":
+        $arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+        $response = json_encode($arr);
         break;
 }
 
-$response = file_get_contents($q);
 echo($response);
-?>
