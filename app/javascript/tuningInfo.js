16a2f3c532daa8b1352d772d2fdfe8a3b824abdc
Christoph <ziegler@irt.de>
2014-09-21 11:15:23 +0200

    tuningInfo.js & solved  time issue in lecon.php

---
+++ app/javascript/tuningInfo.js
@@ -0,0 +1,37 @@
+var tuningInfo = {
+	DAS_ERSTE: {
+		sourceName: 'Das Erste',
+		sourceID: '71',
+		onid: '1',
+		tsid: '1',
+		sid: '1'
+	},
+	PRO_7: {
+		sourceName: 'ProSieben',
+		sourceID; '40',
+		onid: '1',
+		tsid: '1',
+		sid: '2'
+	},
+	ARTE: {
+		sourceName: 'Arte',
+		sourceID: '58',
+		onid: '1',
+		tsid: '1',
+		sid: '3'
+	},
+	SIXX: {
+		sourceName: 'Sixx',
+		sourceID: '694',
+		onid: '1',
+		tsid: '1',
+		sid: '4'
+	},
+	ZDF: {
+		sourceName: 'ZDF',
+		sourceID: '37',
+		onid: '1',
+		tsid: '1',
+		sid: '5'
+	}
+}
